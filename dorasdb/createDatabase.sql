c5350c388729bbe439b50361b29f76236a3465a0
Conor Moss <conormo@conormo.local>
2019-11-05 16:01:47 +0000

    Create Database script

--- dorasdb/createDatabase.sql
+++ dorasdb/createDatabase.sql
@@ -2,5 +2,13 @@ DROP DATABASE dorasdb;
 CREATE DATABASE dorasdb;
 USE DATABASE dorasdb;
 
+SOURCE createCompetencies.sql
+SOURCE createBand.sql
+SOURCE createCompetenciesBand.sql
+SOURCE createTraining.sql
+SOURCE createCapabilityLead.sql
+SOURCE createCapability.sql
+SOURCE createJobFamily.sql
 SOURCE createJob.sql
-SOURCE 
+SOURCE createCompetenciesJob.sql
+SOURCE createTrainingJob.sql
