89b19493a3aea2735fdf5c45af82ce8b0a05e973
Ryan Connolly <ryanco@Centurion.local>
2019-11-05 15:15:51 +0000

    added trainign table creation statement

---
+++ dorasdb/CreateTraining.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Training(
+    tid int PRIMARYKEY AUTO_INCREMENT NOT NULL,
+    name varchar(50) NOT NULL default'Under work',
+    type varchar(255),
+    link varchar(89),
+    description varchar(510) DEFAULT ' '
+)
