3a3a61f482ad9b69c47fad9c43e02472164c6676
Patrick Watson <patrickwa@kainos.com>
2019-11-06 16:17:22 +0000

    i give up

---
+++ ExpressAPI/app.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const app = express()
+const env = require('dotenv').config()
+const db = require('./db.js')
+
+function getBand(bandsReadyFn) {
+    db.getBand(function(rows) {
+        bands = rows
+        bandsReadyFn()
+    })
+}
+app.get('/getBand', function(req, res) {
+    getBand(function() {
+        res.send(bands)
+    })
+})
+
+function getJobTitle(jobReadyFn) {
+  db.getJobTitle(function(rows) {
+    job = rows
+    jobReadyFn()
+  })
+}
+app.get('/getJobTitle', function(req,res) {
+  getJobTitle(function() {
+    res.send(job)
+  })
+})
+
+function getBandId(bandIdReadyFn) {
+  db.getBandId(req.body, function(bandName){
+    bandId = rows
+    bandIdReadyFn()
+  })
+}
+
+app.get('/getBandId', function(req,res) {
+  getBandId(function() {
+    res.send(bandId)
+  })
+})
+
+function getJobIdAndCapId(jobIdCapIDReadyFn) {
+  db.getJobIdAndCapId(req.body, function(capName){
+    jobIdCapId = rows
+    jobIdCapIDReadyFn()
+  })
+}
+app.get('/getJobIdAndCapId', function(req,res) {
+  getJobIdAndCapId(function() {
+    res.send(jobIdCapId)
+  })
+})
+
+app.listen(8003, function() {
+    console.log('Express started')
+})
---
+++ ExpressAPI/db.js
@@ -0,0 +1,53 @@
+const mysql = require('mysql')
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'dorasdb'
+})
+
+db.connect(function(err) {
+    if (err) throw err
+    console.log('Connected to MySQL')
+})
+
+exports.getBand = function(callback) {
+  db.query(
+      "SELECT bandName, bandNo FROM Band",
+      function(err, rows) {
+          if (err) throw err
+          callback(rows)
+      })
+}
+
+exports.getBandId = function(bandName, callback) {
+  db.query(
+    "SELECT bandId FROM Band where bandName = ?",
+    [bandName],
+    function(err, rows) {
+      if (err) throw err
+      callback(rows)
+    })
+}
+
+exports.getJobIdAndCapId = function(capName, callback) {
+  db.query(
+    "SELECT capId, jfId, FROM Capability WHERE capName = ?",
+    [capName],
+    function(err, rows) {
+      if (err) throw err
+      callback(rows)
+    }
+  )
+}
+
+exports.getJobTitle = function(bandId, jfid, callback) {
+  db.query(
+    "Select title FROM Job WHERE jfid = ? AND bandId = ?",
+    [jfid, bandId],
+    function(err, rows) {
+      if (err) throw err
+      callback(rows)
+    }
+  )
+}
