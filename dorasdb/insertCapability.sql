c6e1e4b296d62f697a0b38c2fde182d5e4c97db4
Ryan Connolly <ryanco@Centurion.local>
2019-11-06 09:49:57 +0000

    Added data for capabilities and job families

---
+++ dorasdb/insertCapability.sql
@@ -0,0 +1,7 @@
+INSERT INTO  Capability(capName,leadId)
+VALUES("Technical","1"),
+VALUES("Sales and Marketing","2"),
+VALUES("consulting","3"),
+VALUES("Exprience design","4"),
+VALUES("Management","5"),
+VALUES("Central services team","6")
