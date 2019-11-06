eb5bbf1c6373528c76cd2de7e8c84b5dcad7000c
Ryan Connolly <ryanco@Centurion.local>
2019-11-06 11:36:11 +0000

    changed insertBand

--- dorasdb/insertBand.sql
+++ dorasdb/insertBand.sql
@@ -1,10 +1,10 @@
 INSERT INTO Band(bandNo,bandName)
 VALUES(0,"Executive"),
-VALUES(0,"Leadership Coummunity"),
-VALUES(1,"Principle"),
-VALUES(2,"Manager"),
-VALUES(3,"Consultant"),
-VALUES(4,"Senior Associate"),
-VALUES(5,"Associate"),
-VALUES(6,"Trainiee"),
-VALUES(7,"Apprentice")
+VALUES(1,"Leadership Coummunity"),
+VALUES(2,"Principle"),
+VALUES(3,"Manager"),
+VALUES(4,"Consultant"),
+VALUES(5,"Senior Associate"),
+VALUES(6,"Associate"),
+VALUES(7,"Trainiee"),
+VALUES(8,"Apprentice")
