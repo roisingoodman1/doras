b113cd4dd1c1da456497bb9fa4dc377ba2189c62
Thomas Boyle <thomasboyle@kainos.com>
2019-11-06 12:23:29 +0000

    lol

--- ExpressAPI/db.js
+++ ExpressAPI/db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql')
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'kladderUser',
-    password: 'klu_master',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: 'dorasdb'
 })
 
