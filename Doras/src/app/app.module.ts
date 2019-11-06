9b917c758267eaaac3a2ada6e7e8bc8647f3a469
amyasmith <amys@kainos.com>
2019-11-06 10:10:15 +0000

    added tabs for carousel

--- Doras/src/app/app.module.ts
+++ Doras/src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselTabsComponent } from './carousel-tabs/carousel-tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselTabsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
