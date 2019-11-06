18225aac93c526b048238dd9410bd021d89e66f7
Ryan Connolly <ryanco@Centurion.local>
2019-11-06 10:51:25 +0000

    added the bands data

---
+++ dorasdb/insertBand.sql
@@ -0,0 +1,10 @@
+INSERT INTO Band(bandNo,bandName)
+VALUES(0,"Executive"),
+VALUES(0,"Leadership Coummunity"),
+VALUES(1,"Principle"),
+VALUES(2,"Manager"),
+VALUES(3,"Consultant"),
+VALUES(4,"Senior Associate"),
+VALUES(5,"Associate"),
+VALUES(6,"Trainiee"),
+VALUES(7,"Apprentice")
