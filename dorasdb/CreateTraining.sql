fc222e125bae8d45769c448ff2e5f6eb19547316
Roisin <r.goodman@kainos.com>
2019-11-07 14:18:41 +0000

    updated dummy data to match new field

---
+++ dorasdb/CreateTraining.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Training(
+    tId int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title varchar(50) NOT NULL default'Under work',
+    trainingType varchar(255), /*Mandatory, Elective, Selective*/
+    link varchar(89),
+    trainingDescription varchar(510) DEFAULT ' ',
+    trainingCategory varchar(30) /*Professional skills, development programme, technical skills */
+);
---
+++ dorasdb/insertTraining.sql
@@ -0,0 +1,9 @@
+INSERT INTO Training(title, trainingType, link, trainingDescription, trainingCategory) VALUES
+("Coaching - Foundation","Selective","https://www.pluralsight.com/"," A coaching foundation course", 'Development Program'),
+("People Manager Toolkit: Performance","Mandatory","https://www.pluralsight.com/"," Toolkit for people performance", 'Professional Skills'),
+("Coaching - Intermediate","Elective","https://www.pluralsight.com/","Intermediate coaching training", 'Technical skills'),
+("Presentation Skills","Selective","https://www.pluralsight.com/","How to present well", 'Development program'),
+("Interview Skills - Foundation","Elective","https://www.pluralsight.com/", "Basic interview skills training", 'Technical Skills'),
+("Managing Teams Effectively","Mandatory","https://www.pluralsight.com/","How to manage a team effectively", 'Professional Skills'),
+("Trainee Development Program","Mandatory","https://www.pluralsight.com/","Developing as a trainee", 'Development Program'),
+("Apprentice Development Program","Mandatory","https://www.pluralsight.com/","Developing as an apprentice", 'Technical Skills');
