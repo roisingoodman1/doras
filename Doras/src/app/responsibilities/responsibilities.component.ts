9e9e3773b4756c2b0a336942e9ea374e385671eb
Luke M <lukemcilwaine@outlook.com>
2019-11-08 05:55:56 +0000

    modals fixed

--- Doras/src/app/responsibilities/responsibilities.component.ts
+++ Doras/src/app/responsibilities/responsibilities.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-responsibilities',
@@ -6,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./responsibilities.component.css']
 })
 export class ResponsibilitiesComponent implements OnInit {
+  
+  constructor(private modalService: NgbModal) { }
 
-  constructor() { }
-
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+  }
   ngOnInit() {
   }
 
--- Doras/src/app/training-popup/training-popup.component.ts
+++ Doras/src/app/training-popup/training-popup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-training-popup',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrainingPopupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+  }
 
   ngOnInit() {
   }
