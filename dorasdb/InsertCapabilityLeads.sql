cda6bfc91308cfbc362077d3870351d05eb76227
Conor Moss <conormo@conormo.local>
2019-11-12 14:41:26 +0000

    Further changes, added fields to caplead table

--- dorasdb/InsertCapabilityLeads.sql
+++ dorasdb/InsertCapabilityLeads.sql
@@ -1,7 +1,7 @@
-INSERT INTO CapabilityLead(capLeadName,capLeadPath) VALUES
-("Daniel Dosser","doras/dorasdb/default-profile"),
-("Martha O'Connor","doras/dorasdb/default-profile"),
-("Alex Billy","doras/dorasdb/default-profile"),
-("Russel Mann","doras/dorasdb/default-profile"),
-("Tessa Croisant","doras/dorasdb/default-profile"),
-("Yahweh Jehovah","doras/dorasdb/default-profile");
+INSERT INTO CapabilityLead(capLeadName,capLeadPath, capLeadQuote) VALUES
+("Daniel Dosser","doras/dorasdb/default-profile", "Talent wins games, but teamwork and intelligence wins championships"),
+("Martha O'Connor","doras/dorasdb/default-profile","If something is wrong, fix it now. But train yourself not to worry, worry fixes nothing."),
+("Alex Billy","doras/dorasdb/default-profile","Innovation distinguishes from a leader and a follower"),
+("Russel Mann","doras/dorasdb/default-profile","If your actions inspire others to dream more, learn more, do more and become more, you are a leader."),
+("Tessa Croisant","doras/dorasdb/default-profile","There’s zero correlation between being the best talker and having the best ideas."),
+("Yahweh Jehovah","doras/dorasdb/default-profile","Learning never exhausts the mind.");
